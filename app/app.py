d30f2f1f887ffe659298502f4049663b1283fc8a
boa50 <bruno.albuquerque50@gmail.com>
2020-11-02 11:04:15 -0300

    Estrutura inicial da rede neural

--- app/app.py
+++ app/app.py
@@ -1,8 +1,34 @@
-# import matplotlib
-# matplotlib.use('TkAgg')
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
-plt.show()
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+from tensorflow.compat.v1 import ConfigProto, InteractiveSession
+
+def config_gpu():
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = InteractiveSession(config=config)
+
+def build_model():
+    img_shape = (48, 48, 1)
+
+    img_input = Input(shape=img_shape)
+    x = Conv2D(32, 3, padding='same', activation='relu')(img_input)
+    x = MaxPool2D()(x)
+    x = Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = Flatten()(x)
+    x = Dense(256)(x)
+    out = Dense(7, activation='softmax')(x)
+
+    model = Model(img_input, out) 
+
+    return model
+
+if __name__ == '__main__':
+    config_gpu()
+
+    model = build_model()
+    print(model.summary())
