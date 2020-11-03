a383108dddb6bed2b92c040889ef26ed8d2537ce
boa50 <bruno.albuquerque50@gmail.com>
2020-11-03 09:03:27 -0300

    Treinamento básico dos dados

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 from tensorflow.keras import Input, Model
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.compat.v1 import ConfigProto, InteractiveSession
 
 def config_gpu():
@@ -16,12 +18,12 @@ def prepare_dataset():
     df.columns = ['label', 'usage', 'img']
     df = df.drop(columns=['usage'])
 
-    emotions = ['Raiva', 'Nojo', 'Medo', 'Feliz', 'Triste', 'Surpreso', 'Neutro']
-
-    df['emotion'] = df['label'].apply(lambda x: emotions[int(x)])
     df['img'] = df['img'].apply(lambda x: np.array(x.split(' '), dtype=int).reshape(48, 48, 1))
 
-    return df
+    x = np.array(df['img'].tolist())/255
+    y = to_categorical(np.array(df['label'].tolist()))
+
+    return x, y
 
 def show_img(img):
     plt.figure(figsize=(6, 6))
@@ -30,6 +32,12 @@ def show_img(img):
     plt.axis(False)
 
     plt.show()
+
+def get_emotion(label):
+    emotions = ['Raiva', 'Nojo', 'Medo', 'Feliz', 'Triste', 'Surpreso', 'Neutro']
+    emotion = emotions[int(label)]
+
+    return emotion
 
 def build_model():
     img_shape = (48, 48, 1)
@@ -48,12 +56,43 @@ def build_model():
 
     return model
 
+def train_model(model, epochs=10, batch_size=128, validation_split=0.2, model_name='default', es_patience=10):
+    model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+    model_filepath = f'app/saves/model_{model_name}.h5'
+
+    model_save = ModelCheckpoint(
+        filepath=model_filepath, 
+        save_best_only=True,
+        monitor='val_loss',
+        mode='min',
+        verbose=0)
+
+    es = EarlyStopping(
+        monitor='val_loss',
+        mode='min',
+        patience=es_patience,
+        verbose=1)
+
+    callbacks = [model_save, es]
+
+    model.fit(x, y, epochs=epochs, batch_size=batch_size, validation_split=validation_split, callbacks=callbacks)
+
+
 if __name__ == '__main__':
-    # config_gpu()
+    config_gpu()
 
-    df = prepare_dataset()
+    x, y = prepare_dataset()
+    # idx = 7
+    # print(get_emotion(np.argmax(y[idx])))
+    # show_img(x[idx])
 
-    print(df.head())
+    model = build_model()
+    # print(model.summary())
 
-    show_img(df['img'][5])
-    # model = build_model()
+    epochs = 5
+    batch_size = 256
+
+    train_model(model, epochs=epochs, batch_size=batch_size)
